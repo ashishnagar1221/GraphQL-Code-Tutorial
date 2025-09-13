60ecfe26d45d6c76091c94480f32e7d7f3a9c45c
Ashish <ashishnagar1221@gmail.com>
2025-09-13 12:41:58 +0530

    first commit

---
+++ schema/resolvers.js
@@ -0,0 +1,59 @@
+const { UserList, MovieList } = require("../FakeData");
+
+const resolvers = {
+  Query: {
+    //Users resolvers
+    users: () => {
+      return UserList;
+    },
+    user: (_, args) => {
+      const id = args.id;
+      return UserList.find((ele) => Number(ele.id) === Number(id));
+    },
+
+    //Movie resolvers
+    movies: () => {
+      return MovieList;
+    },
+    movie: (_, args) => {
+      const name = args.name;
+      return MovieList.find((ele) => ele.name.includes(name));
+    },
+  },
+  User: {
+    favoriteMovies: () => {
+      return MovieList.filter(
+        (mov) => mov.yearOfPublication >= 2000 && mov.yearOfPublication <= 2010
+      );
+    },
+  },
+  Mutation: {
+    createUser: (parent, args) => {
+      const user = args.input;
+      const lastId = UserList[UserList.length - 1].id;
+      user.id = lastId + 1;
+      UserList.push(user);
+      return user;
+    },
+
+    updateUserName: (parent, args) => {
+      const { id, newUsername } = args.input;
+      let userUpdated;
+      UserList.forEach((user) => {
+        if (user.id == id) {
+          user.username = newUsername;
+          userUpdated = user;
+        }
+      });
+
+      return userUpdated;
+    },
+    deleteUser: (parent, args) => {
+      const id = args.id;
+      UserList.filter((user) => user.id !== Number(id));
+      return null;
+    },
+  },
+};
+
+module.exports = { resolvers };
