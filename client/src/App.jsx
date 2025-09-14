e676273c7a524c885221b9e1290427cae3f0c6b7
Ashish <ashishnagar1221@gmail.com>
2025-09-14 16:14:35 +0530

    Implement userQuery using apollo client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,21 @@
-import "./App.css";
-import {
-  ApolloClient,
-  InMemoryCache,
-  HttpLink,
-  useQuery,
-} from "@apollo/client";
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 import { ApolloProvider } from "@apollo/client/react";
 import DisplayData from "./DisplayData";
 
+
 function App() {
   const client = new ApolloClient({
-    cache: new InMemoryCache(),
     link: new HttpLink({ uri: "http://localhost:4000/graphql" }),
+    cache: new InMemoryCache(),
   });
+
   return (
-    <>
-      <ApolloProvider client={client}>
-        <div>
-          <h1>List of Users</h1>
-          <DisplayData/>
-        </div>
-      </ApolloProvider>
-    </>
+    <ApolloProvider client={client}>
+      <div>
+        <h1>List of Users</h1>
+        <DisplayData/>
+      </div>
+    </ApolloProvider>
   );
 }
 
--- client/src/DisplayData.jsx
+++ client/src/DisplayData.jsx
@@ -1,5 +1,5 @@
-import { useQuery, gql } from "@apollo/client/react";
-import React from "react";
+import { useQuery } from "@apollo/client/react";
+import { gql } from "@apollo/client";
 
 const QUERY_ALL_USERS = gql`
   query GetAllUsers {
@@ -8,16 +8,32 @@ const QUERY_ALL_USERS = gql`
       name
       age
       username
+      nationality
     }
   }
 `;
-
 const DisplayData = () => {
-  const { loading, data, error } = useQuery(QUERY_ALL_USERS);
-  if (data) {
-    console.log(data);
+  const { data, loading, error } = useQuery(QUERY_ALL_USERS);
+
+  console.log(data);
+  if (loading) {
+    return <h1>Data is loading ...</h1>;
   }
-  return <div></div>;
+  if (error) {
+    console.log(error);
+  }
+  return (
+    <div>
+      {data.users.map((ele) => (
+        <div>
+          <h3>Name : {ele.name}</h3>
+          <h3>Age : {ele.age}</h3>
+          <h3>Username: {ele.username}</h3>
+          <h3>Nationality: {ele.nationality}</h3>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default DisplayData;
